f7ef5b998a05e7f1d0fee0a78ed4b86a7b079441
RBrettes <rui.brettes+dev@gmail.com>
2018-01-10 16:06:59 +0000

    Gen Update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "UPShare";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "011401ff0001978f948be302029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121019d1dd14d380eefab21428e4db392ee0fa02eccb43334f04c83d280ea0733bca5";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
